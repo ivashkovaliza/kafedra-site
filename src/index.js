a7aa7f777e7268b6d0780cc89758948d0515d820
ivashkovaliza <ivashkovaliza@gmail.com>
2018-04-27 16:51:40 +0300

    Create main page

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import "./styles/styles.scss";
+
+$(function(){
+    console.log(window.innerWidth);
+
+
+    let count = 0;
+    $('.drop-down-menu').hide();
+    $('.burger-menu').click(
+        function(){
+            count++;
+            if (count === 1) {
+                $('.drop-down-menu').slideDown();
+            } else {
+                $('.drop-down-menu').slideUp('fast');
+                count = 0;
+            }
+        });
+    window.onresize = function(){
+        if(window.innerWidth >= 1025) {
+            $('.drop-down-menu').hide();
+            count = 0;
+        }
+
+    }
+
+});
+
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+});
+
+
+
+
+
